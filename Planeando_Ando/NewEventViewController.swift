b2097af72807f82a127af3ded06a1f8d9e3775db
Fernando Limón <flimflo2@hotmail.com>
2019-11-10 19:53:43 -0600

    Solucionar problemas en la integración de TableViewController

---
+++ Planeando_Ando/NewEventViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NewEventViewController.swift
+//  Planeando_Ando
+//
+//  Created by Fernando Limón Flores on 10/11/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBAction func infoTextField(_ sender: UITextField) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
