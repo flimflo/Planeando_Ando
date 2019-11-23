90fdc4465fdc79f34f9ffb41baeefb4c3c3e3c8f
Mildred Gil <mi.mitel@hotmail.com>
2019-11-23 14:20:05 -0600

    join button for join event view

---
+++ Planeando_Ando/JoinEventViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JoinEventViewController.swift
+//  Planeando_Ando
+//
+//  Created by Mildred Irais Gil Melchor on 11/22/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class JoinEventViewController: UIViewController {
+
+    @IBOutlet weak var tfKey: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func ValidateKey(key : String) -> Bool {
+        //add the validation keynumber
+        return true
+    }
+    
+    
+    // MARK: - Navigation
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return ValidateKey(key: tfKey.text!)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //send de id event 
+    }
+}
