532b9e19688906192656ecaf0a961fd8987906b1
Mildred Gil <mi.mitel@hotmail.com>
2019-11-27 03:40:04 -0600

    add and edit settings

--- Planeando_Ando/JuntasTableViewController.swift
+++ Planeando_Ando/JuntasTableViewController.swift
@@ -97,32 +97,33 @@ class JuntasTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath)
         let junta = juntasArray[indexPath.row]
-        print("\(junta.description)")
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "MM-dd-yy HH:mm"
+        formatter.string(from: junta.start)
+        formatter.string(from: junta.start)
+        
         cell.textLabel?.text = "\(junta.description)"
+        cell.detailTextLabel?.text = formatter.string(from: junta.start) + " - " + formatter.string(from: junta.end)
 
         return cell
     }
     
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            juntasArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
